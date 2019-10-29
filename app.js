085f717badb637db2772c449c66452dfd9222636
Marius De Allie <spectre4300s@gmail.com>
2019-10-29 07:14:39 -0400

    Update uiController module IIFE to standard function declaration from arrow function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const budgetController = (function() {
 }());
 /** UI controller (view module) **/
-const uiController = (() => {
+const uiController = (function() {
 
 }());
 // GLOBAL APP CONTROLLER - Controller Module.

f92b0eb9e62e1b51ae2aba92f82750432521a673
Marius De Allie <spectre4300s@gmail.com>
2019-11-01 19:27:54 -0400

    Add conditional check within keypress event listener function to check whether keypressed is enter key

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ document.querySelector('.add__btn').addEventListener('click', function() {
   console.log('add button clicked.');
 });
 /* Enter keypress event handler on document object. */
-document.addEventListener('keypress' function(evt) {
+document.addEventListener('keypress', function(evt) {
   console.log('key pressed.');
+  if (evt.keyCode === 13) { // Check whether key pressed is enter key.
+    console.log('Enter key pressed.');
+  }
 });
 }(budgetController, uiController));

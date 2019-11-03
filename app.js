adda9c2d0d1cb1c8640dfb5a5a4d406a5901b032
Marius De Allie <spectre4300s@gmail.com>
2019-11-03 10:17:20 -0500

    Move ctrlAddItem function expression to the top of the controller module

--- app.js
+++ app.js
@@ -17,28 +17,29 @@ const uiController = (function() {
 }());
 /** GLOBAL APP CONTROLLER - Controller Module. **/
 const controller = (function(budgetCtrl, uiCtrl) {
-/* Click event handler on add button */
-document.querySelector('.add__btn').addEventListener('click', function() {
-  console.log('add button clicked.');
-});
-/* Enter keypress event handler on document object. */
-document.addEventListener('keypress', function(evt) {
-  console.log('key pressed.');
-  if (evt.keyCode === 13) { // Check whether key pressed is enter key.
-    console.log('Enter key pressed.');
-  }
-});
-/* Private function to add new item to app. */
-const ctrlAddItem = function() {
-  // 1. Get field input data. *This needs to be done in the uicontroller module.
-  let input = iuCtrl.getInput(); // Call uiController getInput public method and assign returned obj to input var.
-  // 2. Add the item to the budget controller.
+  /* Private function to add new item to app. */
+  const ctrlAddItem = function() {
+    // 1. Get field input data. *This needs to be done in the uicontroller module.
+    let input = iuCtrl.getInput(); // Call uiController getInput public method and assign returned obj to input var.
+    // 2. Add the item to the budget controller.
 
-  // 3. Add the item to UI.
+    // 3. Add the item to UI.
 
-  // 4. Calculate the budget.
+    // 4. Calculate the budget.
 
-  // 5. Display the budget in UI.
+    // 5. Display the budget in UI.
 
-};
+  };
+
+  /* Click event handler on add button */
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    console.log('add button clicked.');
+  });
+  /* Enter keypress event handler on document object. */
+  document.addEventListener('keypress', function(evt) {
+    console.log('key pressed.');
+    if (evt.keyCode === 13) { // Check whether key pressed is enter key.
+      console.log('Enter key pressed.');
+    }
+  });
 }(budgetController, uiController));

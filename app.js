09cde95e96bec4428b44f7b1fb1c44c7998e217c
Marius De Allie <spectre4300s@gmail.com>
2019-11-04 09:45:23 -0500

    Set value property to value of value parameter in Income constructor function

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const budgetController = (function() {
   const Income = function(id, description, value) {
     this.id = id;
     this.description = description;
-
+    this.value = value;
   };
 }());
 /** UI CONTROLLER - View module **/

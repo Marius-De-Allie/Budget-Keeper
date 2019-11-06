7f0cae8f3f1030a46b978303301036bd20db3cc6
Marius De Allie <spectre4300s@gmail.com>
2019-11-06 14:58:31 -0500

    Within uiController.displayBidget method set '.budget__value' DOM element node's textContent to value of budgetController.data.budget

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ const uiController = (function() {
       parentElement.insertAdjacentHTML('beforeend', newHtml); // Add new item html element as the last child of the parent element.
     },
     displayBudget: function(budgetObject) { // Public method to display budget values in UI.
+      document.querySelector('.budget__value').textContent = budgetObject['budget'] // Set '.budget__value' DOM element node textContent value to value stored in budgetController.data.budget.
 
     }
   };

9fa15bf8185992ec11c5cd198d2c2802409b10ca
Marius De Allie <spectre4300s@gmail.com>
2019-11-07 21:59:22 -0500

    Convert 'inputFields' nodelist to an array using Array.from method and assign value to 'inputFieldsArray' within uiController.clearFields method

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ const uiController = (function() {
     },
     clearInputFields: function() { // Public method to clear input fields after new item is added.
         const inputFields = document.querySelectorAll('.add__description', '.add__value'); // Store value of DOM element nodes '.add__description' and '.add__value' in nodeList.
+        const inputFieldsArray = Array.from(inputFields); // Convert inputFields nodelist to an array.
     },
     displayBudget: function(budgetObject) { // Public method to display budget values in UI.
       document.querySelector('.budget__value').textContent = budgetObject['budget']; // Set '.budget__value' DOM element node textContent value to value stored in budgetController.data.budget.
